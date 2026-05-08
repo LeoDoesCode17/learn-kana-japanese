75c98cf7c8bc296c604cee358d4f093a2d990d4a
leonardo <leocode1079@gmail.com>
2026-05-08 16:00:55 +0800

    feat: add vite config for multiple page

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        exercise: resolve(__dirname, "kana_exercise_1.html"),
+      },
+    },
+  },
+});
