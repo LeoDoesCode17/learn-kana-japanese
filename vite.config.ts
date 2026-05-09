2ce7b01ccfe4740a0aefc5797348cd9b3d10755b
leonardo <leocode1079@gmail.com>
2026-05-09 21:36:57 +0800

    feat: add stay tuned page for additional development feature

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         exercise: resolve(__dirname, "kana_exercise_1.html"),
+        staytuned: resolve(__dirname, "stay-tuned.html"),
       },
     },
   },
